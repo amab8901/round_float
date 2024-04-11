ecbb80a3fe974e56c2d69604d4fbc4587d56313f
amab8901 <amab8901@protonmail.com>
2024-04-11 15:26:49 +0200

    remove redundant errors

--- src/round_float.rs
+++ src/round_float.rs
@@ -1,23 +1,26 @@
-use anyhow::Result;
+#![allow(clippy::let_and_return)]
+
 use num_traits::Float;
 pub trait RoundToFraction {
     /// Round `float_number` to specified number of digits in the fraction.
     /// # Errors
     /// Throws error if `digits` is zero
-    fn round_to_fraction(&self, digits: u32) -> Result<Self>
+    fn round_to_fraction(&self, digits: u32) -> Self
     where
         Self: Float,
     {
-        if digits == 0 {
-            return Err(anyhow::Error::msg("`digits` must be a positive integer"));
-        }
+        let rounded_float = if digits == 0 {
+            let rounded_float = self.trunc();
+            rounded_float
+        } else {
+            let ten = Self::from(10.0).unwrap();
+            let digits = Self::from(digits).unwrap();
+            let round_factor = ten * digits;
+            let rounded_float = (self.mul(round_factor)).round() / round_factor;
+            rounded_float
+        };
 
-        let ten = Self::from(10.0).unwrap();
-        let digits = Self::from(digits).unwrap();
-        let round_factor = ten * digits;
-        let rounded_float = (self.mul(round_factor)).round() / round_factor;
-
-        Ok(rounded_float)
+        rounded_float
     }
 }
 
@@ -25,15 +28,18 @@ impl<F> RoundToFraction for F
 where
     F: Float,
 {
-    fn round_to_fraction(&self, digits: u32) -> Result<Self> {
-        if digits == 0 {
-            return Err(anyhow::Error::msg("`digits` must be a positive integer"));
-        }
-        let ten = Self::from(10.0).unwrap();
-        let digits = Self::from(digits).unwrap();
-        let round_factor = ten * digits;
-        let rounded_float = (self.mul(round_factor)).round() / round_factor;
+    fn round_to_fraction(&self, digits: u32) -> Self {
+        let rounded_float = if digits == 0 {
+            let rounded_float = self.trunc();
+            rounded_float
+        } else {
+            let ten = Self::from(10.0).unwrap();
+            let digits = Self::from(digits).unwrap();
+            let round_factor = ten * digits;
+            let rounded_float = (self.mul(round_factor)).round() / round_factor;
+            rounded_float
+        };
 
-        Ok(rounded_float)
+        rounded_float
     }
 }
