1a1fb0da5e19bde9c7fac8f34c17097154f8fced
amab8901 <amab8901@protonmail.com>
2024-04-11 15:17:37 +0200

    Refac: unwrap instead of redundant error handling

--- src/round_float.rs
+++ src/round_float.rs
@@ -3,10 +3,7 @@ use num_traits::Float;
 pub trait RoundToFraction {
     /// Round `float_number` to specified number of digits in the fraction.
     /// # Errors
-    /// Throws error if:
-    /// * can't instantiate `10.0`
-    /// * can't convert `digits` to a Float number
-    /// * `digits` is zero
+    /// Throws error if `digits` is zero
     fn round_to_fraction(&self, digits: u32) -> Result<Self>
     where
         Self: Float,
@@ -14,10 +11,9 @@ pub trait RoundToFraction {
         if digits == 0 {
             return Err(anyhow::Error::msg("`digits` must be a positive integer"));
         }
-        let ten =
-            Self::from(10.0).ok_or(anyhow::Error::msg("Failed to instantiate value `10.0`."))?;
-        let digits =
-            Self::from(digits).ok_or(anyhow::Error::msg("Failed to convert `digits``to float"))?;
+
+        let ten = Self::from(10.0).unwrap();
+        let digits = Self::from(digits).unwrap();
         let round_factor = ten * digits;
         let rounded_float = (self.mul(round_factor)).round() / round_factor;
 
@@ -33,10 +29,8 @@ where
         if digits == 0 {
             return Err(anyhow::Error::msg("`digits` must be a positive integer"));
         }
-        let ten =
-            Self::from(10.0).ok_or(anyhow::Error::msg("Failed to instantiate value `10.0`."))?;
-        let digits =
-            Self::from(digits).ok_or(anyhow::Error::msg("Failed to convert `digits``to float"))?;
+        let ten = Self::from(10.0).unwrap();
+        let digits = Self::from(digits).unwrap();
         let round_factor = ten * digits;
         let rounded_float = (self.mul(round_factor)).round() / round_factor;
 
