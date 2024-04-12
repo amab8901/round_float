26c068d5d9a7fd3383845e5fc4d96a40edb6fed5
amab8901 <amab8901@protonmail.com>
2024-04-12 08:54:28 +0200

    Refac: remove default-overriding round_to_fraction() impl

--- src/round_float.rs
+++ src/round_float.rs
@@ -22,22 +22,4 @@ pub trait RoundToFraction {
     }
 }
 
-impl<F> RoundToFraction for F
-where
-    F: Float,
-{
-    fn round_to_fraction(&self, digits: u32) -> Self {
-        let rounded_float = if digits == 0 {
-            let rounded_float = self.trunc();
-            rounded_float
-        } else {
-            let ten = Self::from(10.0).unwrap();
-            let digits = Self::from(digits).unwrap();
-            let round_factor = ten * digits;
-            let rounded_float = (self.mul(round_factor)).round() / round_factor;
-            rounded_float
-        };
-
-        rounded_float
-    }
-}
+impl<F> RoundToFraction for F where F: Float {}
